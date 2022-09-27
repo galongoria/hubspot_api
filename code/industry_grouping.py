fb62e561cf9219a5771489b4e3b3b33c92be8798
galongoria <george.longoria@utexas.edu>
2022-09-27 17:03:54 -0500

    fixed mapping

--- code/industry_grouping.py
+++ code/industry_grouping.py
@@ -11,14 +11,16 @@ load_dotenv()
 DBDIR = "C:/Users/galon/Sputnik ATX Team Dropbox/Programming Datasets"
 RAW_DIR = os.path.join(DBDIR, "data", "raw")
 CLEAN_DIR = os.path.join(DBDIR, "data", "clean")
+IND_DIR = os.path.join(CLEAN_DIR, "industry_mapping")
 VC_INDUSTRY_COLS = os.path.join(RAW_DIR, "hs", "vc_industry_columns.csv")
-
+STARTPATH = os.path.join(CLEAN_DIR, 'scraped_data', 'crunchbase', 'cb_starts_main_scraped.csv')
+MAPPING_OUTPATH = os.path.join(IND_DIR, "cb_starts_mapped.csv")
 
 """
 Below is a reference for the internal property names in HubSpot:
 
 
-Investment Focus:  inv_inds
+Investment Focus: inv_inds
 Record ID: id
 Company name: name
 Investor Tags: inv_tags
@@ -37,49 +39,72 @@ def make_industry_options(dictionary):
     ]
 
 
-def explode_df(path, old_col, split_col, pattern):
+def explode_df(csv_inpath, industry_column, split_on):
 
     """Takes the dataframe and explodes on the industry"""
+    
+    df = pd.read_csv(csv_inpath).rename(columns={industry_column: 'industries'})
+    df['ref'] = df['industries']
+    df = df[~df['industries'].isnull()]
+    df['industries'] = df['industries'].apply(lambda x: re.split(split_on, str(x)))
+    df = df.explode('industries')
+    df['industries'] = df['industries'].apply(lambda x: re.sub(r"&", "and", str(x)))
+    df['industries'] = df['industries'].apply(lambda x: x.lower().strip())
 
-    df = pd.read_csv(path).rename(columns={old_col: split_col})
-
-    df = df[~df[split_col].isnull()]
-    df[split_col] = df[split_col].apply(lambda x: re.split(pattern, str(x)))
-    df = df.explode(split_col)
-    df[split_col] = df[split_col].apply(lambda x: re.sub(r"&", "and", str(x)))
-    df[split_col] = df[split_col].apply(lambda x: x.lower().strip())
-
-    return df.reset_index(drop=True)
+    return df.drop('Unnamed: 0', axis=1)
 
 
-def map_groups(path, old_col, split_col, new_col, pattern, map_dict):
+def map_groups(exploded_df, new_col, map_dict):
 
     """Takes the exploded dataframe and groups the industries more generally"""
-
-    exploded_df = explode_df(path, old_col, split_col, pattern).set_index("Record ID")
-    all_tags = set(exploded_df[split_col].values)
-    merge_df = exploded_df.reset_index()
-    exploded_df[new_col] = ""
-
+    
+    merge_df = exploded_df.copy(deep=True)
+    merge_df[new_col] = ""
+    exploded_df.reset_index(inplace=True)
+    
     for key, value in map_dict.items():
 
         map_df = pd.DataFrame({key: value})
         map_df[key] = map_df[key].apply(lambda x: x.lower())
-        matches = map_df.merge(
-            merge_df, how="inner", right_on=split_col, left_on=key
-        ).set_index("Record ID")
-        exploded_df.loc[matches.index, new_col] += f";{key}"
+        matches = exploded_df.merge(map_df, how="left", left_on='industries', right_on=key)
+        merge_df.loc[matches[~matches[key].isnull()]['index'].values, new_col] += f";{key}"
+    return merge_df
 
-    grouped = exploded_df.groupby("Record ID").agg(
-        {new_col: lambda x: ("".join(str(s.strip()) for s in set(x))).strip()}
-    )
 
-    return pd.read_csv(path).merge(
-        grouped[grouped[new_col].str.contains(";")],
-        how="left",
-        left_on="Record ID",
-        right_index=True,
-    )
+def aggregate(df, dictionary):
+    
+    
+    return df.groupby(df.index).agg(dictionary)
+
+
+def mental_health(df):
+    
+    df.loc[df['description'].str.contains('mental health'), 'inv_inds'] += ';Mental Health'
+
+    return df
+
+def map_subindustries(ind_list):
+    
+    ind_tuples = [
+        ("Mental Health", "Health"), 
+        ("Payment Card Industry (PCI)", "FinTech"), 
+        ("FinTech","Financial Services"),
+        ("InsurTech", "Insurance"),
+        ("HealthTech", "Health"),
+        ("Enterprise Software", "Software"),
+        ("Streaming Platform", "Arts and Entertainment"),
+        ("Transportation", "Automotive"),
+        ("Pharmaceuticals", "Health"),
+        ("3D Printing", "Manufacturing"),
+        ("E-Commerce", "Commerce"),
+        ("Multimedia and Graphics Software","Software"),
+        ("AgTech", "Agriculture and Farming"),
+        ]
+    
+    for i in ind_tuples:
+        if i[0] in ind_list and i[1] in ind_list:
+            ind_list = [ind for ind in ind_list if ind != i[1]]
+    return ";".join(ind_list)
 
 
 def combine_tables(internal, new_col):
@@ -135,7 +160,26 @@ def rate_limit_dict(client, rate, dict_list):
                     print(f"Just updated batches {i}-{i+rate} \n Sleeping shhhh...")
                     time.sleep(10)
 
+def main():
+
+    df = map_groups(explode_df(STARTPATH, 'industries', ','), 'inv_inds', make_ind_dict())
+    df = aggregate(df, {
+            'inv_inds': lambda x: ("".join(str(s.strip()) for s in set(x))).strip(), 
+            'href': lambda x: ''.join([s for s in set(x)]),
+            'name': lambda x: ''.join([s for s in set(x)]),
+            'description': lambda x: ''.join([s for s in set(x)]),
+            'ref': lambda x: ''.join([s for s in set(x)]),
+    })
+    df = mental_health(df)
+    df['inv_inds'] = df['inv_inds'].apply(lambda x: x.split(';'))
+    df['inv_inds'] = df['inv_inds'].apply(lambda x: map_subindustries(x))
+    return df
+
+
+
 
 if __name__ == "__main__":
 
-    client = hubspot.Client.create(access_token=os.getenv("pm_token"))
+    main().to_csv(MAPPING_OUTPATH)
+
+    # client = hubspot.Client.create(access_token=os.getenv("pm_token"))
